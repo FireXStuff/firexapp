a5ddea83c99e74aab8835bd6395e711d464b6dd2
Marc S Delahousse <mdelahou@cisco.com>
2019-01-31 13:26:22 -0500

    submit runs had a dedicated logs_dir, to collect debugging information

--- firexapp/submit.py
+++ firexapp/submit.py
@@ -1,8 +1,10 @@
+import json
 import os
 import re
 import datetime
 import pytz
 import argparse
+import tempfile
 from getpass import getuser
 from firexapp.plugins import plugin_support_parser
 
@@ -25,10 +27,17 @@ class SubmitBaseApp:
         return self.parser
 
     def run_submit(self, args, others):
-        pass
+        chain_args = self.process_other_chain_args(args, others)
 
-    def process_other_chain_args(self, other_args)-> {}:
+        uid = Uid()
+        chain_args['uid'] = uid
+        print('Logs: %s' % uid.logs_dir)  # todo: use logger
 
+        # Create an env file
+        with open(os.path.join(uid.logs_dir, "environ.json"), 'w') as f:
+            json.dump(dict(os.environ), fp=f, skipkeys=True, sort_keys=True, indent=4)
+
+    def process_other_chain_args(self, args, other_args)-> {}:
         chain_args = {}
         try:
             chain_args = get_chain_args(other_args)
@@ -46,6 +55,22 @@ class Uid(object):
             self.identifier = identifier
         else:
             self.identifier = 'FireX-%s-%s-%s' % (self.user, self.timestamp.strftime("%y%m%d-%H%M%S"), os.getpid())
+        self._logs_dir = None
+
+    @property
+    def base_logging_dir(self):
+        return tempfile.gettempdir()
+
+    @property
+    def logs_dir(self):
+        if not self._logs_dir:
+            self._logs_dir = self.create_logs_dir()
+        return self._logs_dir
+
+    def create_logs_dir(self):
+        path = os.path.join(self.base_logging_dir, self.identifier)
+        os.makedirs(path, 0o766)
+        return path
 
     def __str__(self):
         return self.identifier
