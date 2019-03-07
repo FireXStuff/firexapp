a214ed16d9c2a0b06d881e065d2aa3ee234c994a
Marc S Delahousse <mdelahou@cisco.com>
2019-03-07 14:03:46 -0500

    Refactory build.py to have ability to run sub steps of the build process

--- build.py
+++ build.py
@@ -32,11 +32,11 @@ def get_git_hash_tags_and_files(workspace):
         print('Git tags @ HEAD: %s' % git_tags)
     files = [file.decode() for file in check_output(['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD'],
                                                     cwd=workspace).splitlines()]
-    print ('Files @ HEAD: %s' % '\n'.join(files))
+    print('Files @ HEAD: %s' % '\n'.join(files))
     return git_hash, git_tags, files
 
 
-def run_tests(workspace):
+def run_unit_tests(workspace):
     print('--> Run unit-tests and coverage')
     check_call(['coverage', 'run', '-m', 'unittest', 'discover', '-s', 'test/', '-p', '*_tests.py'], cwd=workspace)
 
@@ -46,7 +46,9 @@ def upload_coverage_to_codecov(workspace):
     check_call(['codecov'], cwd=workspace)
 
 
-def generate_htmlcov(workspace, git_hash):
+def generate_htmlcov(workspace, git_hash=None):
+    if not git_hash:
+        git_hash, _, __ = get_git_hash_tags_and_files(workspace)
     print('--> Generate the coverage html')
     check_call(['coverage', 'html', '--title', 'Code Coverage for %s' % git_hash], cwd=workspace)
     print('View Coverage at: %s' % os.path.abspath(os.path.os.path.join(workspace, 'htmlcov/index.html')))
@@ -70,7 +72,7 @@ def run(workspace='.', skip_build=None, upload_pip=None, upload_pip_if_tag=None,
     if not skip_build:
         build(workspace)
 
-    run_tests(workspace)
+    run_unit_tests(workspace)
 
     if not skip_htmlcov:
         generate_htmlcov(workspace, git_hash)
@@ -85,19 +87,44 @@ def run(workspace='.', skip_build=None, upload_pip=None, upload_pip_if_tag=None,
         build_sphinx_docs(workspace)
 
 
-if __name__ == '__main__':
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--workspace', default='.')
-    parser.add_argument('--skip_build', action='store_true')
-    parser.add_argument('--upload_pip', action='store_true')
-    parser.add_argument('--upload_pip_if_tag', action='store_true')
-    parser.add_argument('--twine_username', default='firexdev')
-    parser.add_argument('--skip_htmlcov', action='store_true')
-    parser.add_argument('--upload_codecov', action='store_true')
-    parser.add_argument('--skip_docs_build', action='store_true')
+    parser.set_defaults(func=run)
+    sub_parser = parser.add_subparsers()
+    do_all = sub_parser.add_parser("all")
+    do_all.add_argument('--skip_build', action='store_true')
+    do_all.add_argument('--upload_pip', action='store_true')
+    do_all.add_argument('--upload_pip_if_tag', action='store_true')
+    do_all.add_argument('--twine_username', default='firexdev')
+    do_all.add_argument('--skip_htmlcov', action='store_true')
+    do_all.add_argument('--upload_codecov', action='store_true')
+    do_all.add_argument('--skip_docs_build', action='store_true')
+    do_all.set_defaults(func=run)
+
+    upload = sub_parser.add_parser("upload_pip")
+    upload.add_argument('--twine_username', default='firexdev')
+    upload.set_defaults(func=upload_pip_pkg_to_pypi)
+
+    functions = {
+        "build": build,
+        "unit_test": run_unit_tests,
+        "cov_report": generate_htmlcov,
+        "upload_codecov": upload_coverage_to_codecov,
+        "docs": build_sphinx_docs
+    }
+    for name, func in functions.items():
+        sub = sub_parser.add_parser(name)
+        sub.set_defaults(func=func)
 
     args, unknown = parser.parse_known_args()
 
-    run(**vars(args))
+    arguments = dict(vars(args))
+    arguments.pop("func", "")
+    args.func(**arguments)
+
+
+if __name__ == '__main__':
+    main()
