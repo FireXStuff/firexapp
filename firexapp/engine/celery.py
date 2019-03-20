7b43d0530a1b7dd9de961750d8d8d50e78ad66b2
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-03-20 14:04:18 -0400

    Setting the default base class to FireXTask

--- firexapp/engine/celery.py
+++ firexapp/engine/celery.py
@@ -1,5 +1,6 @@
 from celery.app.base import Celery
+from firexkit.task import FireXTask
 
 firex_app_name = '.'.join(__name__.split(".")[:-1])
-app = Celery(strict_typing=False)
+app = Celery(strict_typing=False, task_cls='%s:%s' % (FireXTask.__module__, FireXTask.__name__))
 app.config_from_object(firex_app_name + '.default_celery_config')
