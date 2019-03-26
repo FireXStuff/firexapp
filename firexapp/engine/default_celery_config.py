bba9e1ca2c723fa1080992e8947c8288e137151d
Marc S Delahousse <mdelahou@cisco.com>
2019-03-26 16:42:37 -0400

    Discovery of default tasks

--- firexapp/engine/default_celery_config.py
+++ firexapp/engine/default_celery_config.py
@@ -1,13 +1,17 @@
+import os
+
 from firexapp.broker_manager.broker_factory import BrokerFactory
 from firexapp.plugins import get_plugin_module_list
-from firexapp.discovery import find_firex_task_bundles
+from firexapp.discovery import find_firex_task_bundles, discover_package_modules
 
 
 broker_url = BrokerFactory.get_broker_url()
 result_backend = broker_url
 
+# find default tasks
+default_tasks = os.path.join(os.path.dirname(os.path.dirname(__file__)), "tasks")
 bundles = find_firex_task_bundles()
-imports = tuple(bundles) + tuple(get_plugin_module_list())
+imports = tuple(bundles) + tuple(get_plugin_module_list()) + tuple(discover_package_modules(default_tasks))
 
 accept_content = ['pickle']
 task_serializer = 'pickle'
@@ -17,4 +21,3 @@ result_expires = None
 
 task_acks_late = True
 worker_prefetch_multiplier = 1
-
