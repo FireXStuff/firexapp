24b67bf37cda4a9fbb76e63f7a404dd5dfd2f605
Marc S Delahousse <mdelahou@cisco.com>
2019-04-05 14:25:32 -0400

    Additional logging message to help triage an issue

--- firexapp/engine/default_celery_config.py
+++ firexapp/engine/default_celery_config.py
@@ -1,12 +1,18 @@
+from celery.utils.log import get_task_logger
+
 from firexapp.broker_manager.broker_factory import BrokerFactory
 from firexapp.plugins import get_plugin_module_list
 from firexapp.discovery import find_firex_task_bundles
+
+
+logger = get_task_logger(__name__)
 
 
 broker_url = BrokerFactory.get_broker_url()
 result_backend = broker_url
 
 # find default tasks
+logger.debug("Beginning bundle discovery")
 bundles = find_firex_task_bundles()
 imports = tuple(bundles) + tuple(get_plugin_module_list()) + tuple(["firexapp.tasks.example",
                                                                     "firexapp.tasks.core_tasks"])
