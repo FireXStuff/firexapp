b211b67947d0be1c89336cda58c89bbf8c70ec11
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-04-08 13:44:05 -0400

    Adding debug messages

--- firexapp/engine/default_celery_config.py
+++ firexapp/engine/default_celery_config.py
@@ -3,8 +3,6 @@ from celery.utils.log import get_task_logger
 from firexapp.broker_manager.broker_factory import BrokerFactory
 from firexapp.plugins import get_plugin_module_list
 from firexapp.discovery import find_firex_task_bundles
-
-
 logger = get_task_logger(__name__)
 
 
@@ -14,6 +12,8 @@ result_backend = broker_url
 # find default tasks
 logger.debug("Beginning bundle discovery")
 bundles = find_firex_task_bundles()
+logger.debug("Bundle discovery completed")
+
 imports = tuple(bundles) + tuple(get_plugin_module_list()) + tuple(["firexapp.tasks.example",
                                                                     "firexapp.tasks.core_tasks"])
 
