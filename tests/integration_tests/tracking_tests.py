30b0c0e6a4b3247f263a437eaa0a182b17388e44
Marc S Delahousse <mdelahou@cisco.com>
2019-04-30 16:19:43 -0400

    Fixing test, and enhancing to cover ability to add to the chain_args

--- tests/integration_tests/tracking_tests.py
+++ tests/integration_tests/tracking_tests.py
@@ -1,3 +1,5 @@
+
+from firexapp.engine.celery import app
 from firexapp.submit.arguments import whitelist_arguments
 from firexapp.submit.tracking_service import TrackingService, get_tracking_services
 import firexapp.submit.tracking_service
@@ -7,17 +9,23 @@ from firexapp.testing.config_base import FlowTestConfiguration, assert_is_good_r
 class TestService(TrackingService):
     test_message = "Test service was started"
 
-    def start(self, args)->{}:
+    def start(self, args, **kwargs)->{}:
         print(self.test_message)
+        return {"service_success_value": True}
 
 
 existing_services = get_tracking_services()
 firexapp.submit.tracking_service._services = tuple(list(existing_services) + [TestService()])
 
 
+@app.task
+def service_success(service_success_value=False):
+    assert service_success_value is True
+
+
 class TrackingServiceTest(FlowTestConfiguration):
     def initial_firex_options(self) -> list:
-        return ["submit", "--chain", "nop"]
+        return ["submit", "--chain", "service_success"]
 
     def assert_expected_firex_output(self, cmd_output, cmd_err):
         assert TestService.test_message in cmd_output
