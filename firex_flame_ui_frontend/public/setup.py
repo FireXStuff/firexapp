00708b5766e8fba9fd641928142ccd2f7bd16d92
Dash Jungic <djungic@cisco.com>
2019-05-31 10:00:02 +0100

    Moves python files to public folder. One step towards automating UI python packaging.

--- firex_flame_ui_frontend/public/setup.py
+++ firex_flame_ui_frontend/public/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='firex_flame_ui',
       # version=versioneer.get_version(),
-      version='0.3',
+      version='0.5',
       # cmdclass=versioneer.get_cmdclass(),
       description='UI for FireX.',
       url='https://github.com/FireXStuff/firex-flame-ui',
@@ -11,12 +11,14 @@ setup(name='firex_flame_ui',
       author_email='firex-dev@gmail.com',
       license='BSD-3-Clause',
       packages=['firex_flame_ui'],
-      # npm run build puts artifact in the 'dist' directory.
-      package_dir={'firex_flame_ui': 'dist'},
+      # 'npm run build' is configured to put build artifacts & files in this 'public' folder in the 'dist' directory.
+      # Therefore, at python packaging time, both python files & UI build artifacts are in the same (current) directory.
+      package_dir={'firex_flame_ui': './'},
       zip_safe=True,
       include_package_data=True,
       package_data={
-        'firex_flame_ui': ['*.html', 'js/*.js', 'img/*', 'css/*.css'],
+            # Files available to consumers of this python package (UI build artifacts).
+            'firex_flame_ui': ['*.html', 'js/*.js', 'img/*', 'css/*.css'],
       },
       entry_points={},
       )
