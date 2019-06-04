0560e3bd989e681e2efa672bc8b1fd4d34306d4a
Dash Jungic <djungic@cisco.com>
2019-06-04 13:29:32 +0100

    Make version kludging handle exact tag pushes.

--- firex_flame_ui_frontend/public/setup.py
+++ firex_flame_ui_frontend/public/setup.py
@@ -3,6 +3,8 @@ import versioneer
 
 
 def drop_dirty_if_exact_tag(version):
+    if '+' not in version:
+        return version
     # '0.10', '0.gcb0a42d.dirty' = '0.10+0.gcb0a42d.dirty'.split('+')
     tag, subversion = version.split('+')
     # ['0', 'gcb0a42d'. 'dirty']
