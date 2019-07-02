9829562c0a0a0409fdb387fd88b26f37196e31d7
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-07-02 17:36:39 -0400

    Disabling worker heartbeats; they have been just generating traffic

--- firexapp/celery_manager.py
+++ firexapp/celery_manager.py
@@ -192,7 +192,7 @@ class CeleryManager(object):
         self.pid_files[workername] = pid_file
 
         cmd = '%s worker --hostname=%s@%%h --app=%s --loglevel=%s ' \
-              '--logfile=%s --pidfile=%s --events -Ofair' % (self.celery_bin, workername,
+              '--logfile=%s --pidfile=%s --events --without-heartbeat -Ofair' % (self.celery_bin, workername,
                                                              app, worker_log_level, log_file, pid_file)
         if queues:
             cmd += ' --queues=%s' % queues
