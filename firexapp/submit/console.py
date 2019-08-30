deb9d4748437a30750cdd7becbb8e918822ea0a0
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-08-30 11:46:47 -0400

    Revert "Adding colors to console logging"
    This reverts commit c678ecfefc1d7afc377abc727f0b9859defc7eaa.

--- firexapp/submit/console.py
+++ firexapp/submit/console.py
@@ -1,6 +1,7 @@
-import logging
-import colorlog
 import sys
+import logging
+CONSOLE_LOGGING_FORMATTER = '[%(asctime)s] %(message)s'
+
 
 class DistlibWarningsFilter(logging.Filter):
     def filter(self, record):
@@ -8,23 +9,10 @@ class DistlibWarningsFilter(logging.Filter):
         return not pathname.endswith('distlib/metadata.py') and not pathname.endswith('distlib/database.py')
 
 
-def setup_console_logging(module='',
-                          stdout_logging_level=logging.INFO,
-                          console_logging_formatter='%(green)s[%(asctime)s]%(reset)s %(log_color)s%(message)s',
-                          console_datefmt="%H:%M:%S",
-                          stderr_logging_level=logging.ERROR,
-                          module_logger_logging_level=logging.DEBUG):
-
+def setup_console_logging(module=''):
+    formatter = logging.Formatter(CONSOLE_LOGGING_FORMATTER, "%H:%M:%S")
     module_logger = logging.getLogger(module)
-    module_logger.setLevel(module_logger_logging_level)
-
-    formatter = colorlog.ColoredFormatter(fmt=console_logging_formatter,
-                                          datefmt=console_datefmt,
-                                          log_colors={'DEBUG': 'cyan',
-                                                      'INFO': 'bold',
-                                                      'WARNING': 'yellow',
-                                                      'ERROR': 'red',
-                                                      'CRITICAL': 'red,bg_white'})
+    module_logger.setLevel(logging.DEBUG)
 
     class LogLevelFilter(logging.Filter):
         """Filters (lets through) all messages with level < LEVEL"""
@@ -39,16 +27,15 @@ def setup_console_logging(module='',
             return record.levelno < self.level
 
     console_stdout = logging.StreamHandler(sys.stdout)
-    console_stdout.setLevel(stdout_logging_level)
+    console_stdout.setLevel(logging.INFO)
     console_stdout.setFormatter(formatter)
-    log_filter = LogLevelFilter(stderr_logging_level)
+    log_filter = LogLevelFilter(logging.ERROR)
     console_stdout.addFilter(log_filter)
     console_stdout.addFilter(DistlibWarningsFilter())
 
     console_stderr = logging.StreamHandler()
-    console_stderr.setLevel(stderr_logging_level)
+    console_stderr.setLevel(logging.ERROR)
     console_stderr.setFormatter(formatter)
     module_logger.addHandler(console_stdout)
     module_logger.addHandler(console_stderr)
-
     return module_logger
