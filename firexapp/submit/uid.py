25ce462d71338e50c1fe216266f976817c0afee8
Mohamed Abou El Saoud <mabouels@cisco.com>
2019-09-13 15:24:02 -0400

    Back to Lazy for to prevent IO in constructor, but, cleaned up base_logging_dir

--- firexapp/submit/uid.py
+++ firexapp/submit/uid.py
@@ -17,13 +17,37 @@ class Uid(object):
             self.identifier = identifier
         else:
             self.identifier = 'FireX-%s-%s-%s' % (self.user, self.timestamp.strftime("%y%m%d-%H%M%S"), os.getpid())
-        self.base_logging_dir = tempfile.gettempdir()
-        self.logs_dir = self.create_subdir(self.base_logging_dir, self.identifier)
-        self.debug_dir = self.create_subdir(self.logs_dir, self.debug_dirname)
+        self._base_logging_dir = None
+        self._logs_dir = None
+        self._debug_dir = None
 
-    @staticmethod
-    def create_subdir(base_dir, subdirname):
-        path = os.path.join(base_dir, subdirname)
+    @property
+    def base_logging_dir(self):
+        if not self._base_logging_dir:
+            self._base_logging_dir = tempfile.gettempdir()
+        return self._base_logging_dir
+
+
+    @property
+    def logs_dir(self):
+        if not self._logs_dir:
+            self._logs_dir = self.create_logs_dir()
+            self._debug_dir = self.create_debug_dir()
+        return self._logs_dir
+
+    @property
+    def debug_dir(self):
+        if not self._debug_dir:
+            self._debug_dir = self.create_debug_dir()
+        return self._debug_dir
+
+    def create_logs_dir(self):
+        path = os.path.join(self.base_logging_dir, self.identifier)
+        os.makedirs(path, 0o777)
+        return path
+
+    def create_debug_dir(self):
+        path = os.path.join(self.logs_dir, self.debug_dirname)
         os.makedirs(path, 0o777)
         return path
 
