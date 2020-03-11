51b2440dbdbf2ba602eefa47d149a78bc3830cca
Damir Jungic <djungic@cisco.com>
2020-03-11 10:34:42 -0700

    Fix revoke test in GCP.

--- tests/integration_tests/revoke_tests.py
+++ tests/integration_tests/revoke_tests.py
@@ -4,7 +4,7 @@ from firex_keeper import task_query
 
 from firexapp.engine.celery import app
 from firexapp.submit.submit import get_log_dir_from_output
-from firexapp.testing.config_base import FlowTestConfiguration, assert_is_bad_run
+from firexapp.testing.config_base import FlowTestConfiguration, assert_is_good_run
 
 
 @app.task()
@@ -17,10 +17,12 @@ def revoke(root_uuid):
 @app.task(bind=True)
 def revoke_root_via_child(self):
     root_uuid = self.request.parent_id
-    self.enqueue_child(revoke.s(root_uuid=root_uuid))
+    self.enqueue_child(revoke.s(root_uuid=root_uuid), block=True)
 
 
 class RevokeOnShutdown(FlowTestConfiguration):
+    sync = False
+
     def initial_firex_options(self) -> list:
         return ["submit", "--chain", "revoke_root_via_child"]
 
@@ -33,4 +35,4 @@ class RevokeOnShutdown(FlowTestConfiguration):
         assert revoked_task_count == 3, f"Not all 3 tasks were revoked, was: {revoked_task_count}"
 
     def assert_expected_return_code(self, ret_value):
-        assert_is_bad_run(ret_value)
+        assert_is_good_run(ret_value)
