e86477dc2cf2933f55870e45f2629842fbdcacf2
Damir Jungic <djungic@cisco.com>
2020-12-16 18:23:24 -0800

    Use install configs for log HTML header links.

--- firexapp/engine/celery.py
+++ firexapp/engine/celery.py
@@ -15,11 +15,12 @@ app.config_from_object(firex_app_name + '.default_celery_config')
 def add_uid_to_conf(conf=None, **_kwargs):
     conf.uid = app.backend.get('uid').decode()
     conf.logs_dir = app.backend.get('logs_dir').decode()
-    conf.link_for_logo = conf.logs_dir
     conf.resources_dir = app.backend.get('resources_dir').decode()
     # This should always be the case
     if os.path.isfile(install_config_path_from_logs_dir(conf.logs_dir)):
         conf.install_config = load_existing_install_configs(conf.uid, conf.logs_dir)
+        conf.link_for_logo = conf.install_config.get_logs_root_url()
     else:
         # TODO: consider failing here. Submit process should always populate this.
         conf.install_config = None
+        conf.link_for_logo = conf.logs_dir
