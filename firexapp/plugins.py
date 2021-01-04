9030d9dbc9a78e310671ac535e818a8d8e32af63
Mohamed Abou El Saoud <mabouels@cisco.com>
2021-01-04 16:58:17 -0500

    Adding support for --bringup_only_plugins

--- firexapp/plugins.py
+++ firexapp/plugins.py
@@ -220,9 +220,11 @@ def get_active_plugins():
     return os.environ.get(PLUGGING_ENV_NAME, "")
 
 
-def merge_plugins(plugins_list1, plugins_list2='')->[]:
-    """Merge two comma delimited list of plugins into a single list. Right-handed most significant plugin"""
-    combined_list = delimit2list(plugins_list1) + delimit2list(plugins_list2)
+def merge_plugins(*plugin_lists) -> []:
+    """Merge  comma delimited lists of plugins into a single list. Right-handed most significant plugin"""
+    combined_list = []
+    for plugin_list in plugin_lists:
+        combined_list += delimit2list(plugin_list)
     new_list = []
     for next_idx, plugin in enumerate(combined_list, start=1):
         if plugin not in combined_list[next_idx:]:
