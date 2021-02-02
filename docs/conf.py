7f678a98e397e81a783124cc84bc70b6e387801e
Mohamed Abou El Saoud <mabouels@cisco.com>
2021-02-02 14:30:27 -0500

    Adding sitemap generation

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_sitemap',
 ]
+
+html_baseurl = 'http://www.firexstuff.com/firexapp/'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
