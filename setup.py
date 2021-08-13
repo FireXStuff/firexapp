f0d20b45760794fc84c0ff6fa00c5acb4193857c
Mohamed Abouelsaoud <mabouels@cisco.com>
2021-08-13 14:37:20 -0400

    correct entrypoint info

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='firexapp',
           "License :: OSI Approved :: BSD License",
       ],
       entry_points={
-          'firex.core': 'firex-cisco = firex_cisco',
+          'firex.core': 'firexapp = firexapp',
           'console_scripts': ['firexapp = firexapp.application:main',
                               'flow_tests = firexapp.testing.test_infra:default_main',
                               'firex_shutdown = firexapp.submit.shutdown:main',
