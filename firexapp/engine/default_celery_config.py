d0b24eb34909afa2fed2f52e2b5ee29b271b7b7f
Leo Mermelstein <lmermels@cisco.com>
2022-02-09 12:35:30 -0500

    Fix for autoscaler not taking into acount revoked tasks which are still running

--- firexapp/engine/default_celery_config.py
+++ firexapp/engine/default_celery_config.py
@@ -1,4 +1,11 @@
+from collections.abc import Iterable
+from time import time
+from functools import lru_cache
+
+import celery.worker.state as state
+from celery.worker.autoscale import Autoscaler
 from celery.utils.log import get_task_logger
+from firexkit.result import get_task_postrun_info
 
 from firexapp.broker_manager.broker_factory import BrokerFactory
 from firexapp.engine.logging import add_hostname_to_log_records, add_custom_log_levels, PRINT_LEVEL_NAME
@@ -26,6 +33,46 @@ def _worker_active_monkey_patch(self, worker):
 # Apply the monkey patch
 billiard.pool.Pool._worker_active = _worker_active_monkey_patch
 # End of Monkey Patch
+
+
+# Below code needed to monkey-patch the autoscaler's qty() property
+@lru_cache(maxsize=4096)
+def _get_task_postrun_info(result, _call_time=None):
+    # NOTE: _call_time arg is just used to invalidate the cache
+    return get_task_postrun_info(result)
+
+
+class _MemorizedTasksDone:
+    def __init__(self, check_freq=63):
+        self._check_freq = check_freq
+        self._tasks_done = set()
+
+    def tasks_done(self, results):
+        call_time = int(time()) // self._check_freq
+        if isinstance(results, (str, bytes)) or not isinstance(results, Iterable):
+            results = [results]
+        results = {str(result) for result in results}
+
+        not_done = results - self._tasks_done
+        done = {result for result in not_done if _get_task_postrun_info(result, call_time)}
+        self._tasks_done.update(done)
+        return frozenset(self._tasks_done)
+
+
+_mtd = _MemorizedTasksDone()
+
+
+# We need to make the autoscaler count revoked tasks if they are still running; otherwise
+# it may never scale up if a revoked task schedules another task (from a finally: block, for example)
+# Celery counts revoked tasks as DONE, and therefore it will not know these tasks are still runing
+# and using a worker process
+def _monkey_patch_autoscaler_qty(_self):
+    return len(state.reserved_requests) + len(state.revoked) - len(_mtd.tasks_done(state.revoked))
+
+
+setattr(Autoscaler, 'qty', property(_monkey_patch_autoscaler_qty))
+# End monkey-patching qty()
+
 
 add_custom_log_levels()
 add_hostname_to_log_records()
